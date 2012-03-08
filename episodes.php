5241590d98bcd32080ce3c81eeae4a8d7a156534
Alexis Carlson <whatisthehumanspirit@gmail.com>
2012-03-08 00:20:56 -0800

    Switched back to iframe Like Button.

--- episodes.php
+++ episodes.php
@@ -94,8 +94,7 @@
             
             <p><?php echo $episodes[$currentEpisodeIndex]['summary']; ?></p>
             
-            <!--<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Flocalbrew206&amp;send=false&amp;layout=standard&amp;width=192&amp;height=80&amp;show_faces=true&amp;action=like&amp;colorscheme=dark&amp;font=arial&amp;appId=280528758643105" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:192px; height:80px;" allowTransparency="true"></iframe>-->
-            <div class="fb-like" data-href="<?php echo $ogUrl; ?>" data-send="false" data-width="192" data-show-faces="false" data-colorscheme="dark" data-font="arial"></div>
+            <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Flocalbrew206&amp;send=false&amp;layout=standard&amp;width=192&amp;height=80&amp;show_faces=true&amp;action=like&amp;colorscheme=dark&amp;font=arial&amp;appId=280528758643105" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:192px; height:80px;" allowTransparency="true"></iframe>
           </div>
           
           <div class="clear"></div>
